7ab7a51c8b2657fc9771d8d7dbd434da280f1d14
mrh2v <soolee.h2v@gmail.com>
2017-07-05 22:53:48 +0700

    7/5

--- public/js/module.js
+++ public/js/module.js
@@ -1,21 +1,27 @@
-var app = angular.module("appTinTuc", ['ui.bootstrap', 'ui.router']);
+var app = angular.module("appTinTuc", ['ui.bootstrap', 'ui.router', 'ngAnimate']);
 
 app.config(function($stateProvider, $locationProvider) {
-  var adminState = {
-    name: 'admin',
-    url: '/quan-li',
-    templateUrl: 'views/quan-li.html',
-    controller: "quanliCtrl"
-  }
 
   $locationProvider.html5Mode(true);
   $locationProvider.hashPrefix('');
-  $stateProvider.state(adminState);
+  $stateProvider
+    .state('admin', {
+      url: '/quan-li-he-thong',
+      templateUrl: 'views/quan-li.html',
+      controller: "quanliCtrl"
+    })
+    .state('admin.ttchitiet', {
+      url: '/thong-tin-ca-nhan/chi-tiet/:id',
+      templateUrl: 'views/thong-tin-ca-nhan.html',
+      controller: "ttcnCtrl"
+    })
+    .state('admin.taikhoan', {
+      url: '/thong-tin-ca-nhan/tai-khoan/:id',
+      templateUrl: 'views/thong-tin-ca-nhan.html',
+      controller: "tkCtrl"
+    })
 });
 
-
 app.run(function($rootScope, $state) {
-
   $state.go('admin');
-
 })
--- public/js/quanli.js
+++ public/js/quanli.js
@@ -1,3 +1,14 @@
-angular.module("appTinTuc").controller('quanliCtrl', function( $state, $rootScope, $scope){
+angular.module("appTinTuc").controller('quanliCtrl', function($state, $rootScope, $scope) {
   console.log('vao');
+  $scope.goChiTiet = function() {
+    $state.go("admin.ttchitiet", {id: 123});
+  }
+})
+angular.module("appTinTuc").controller('ttcnCtrl', function($state, $rootScope, $scope) {
+  console.log('vao day');
+
+})
+angular.module("appTinTuc").controller('tkCtrl', function($state, $rootScope, $scope) {
+  console.log('vao day');
+  
 })
